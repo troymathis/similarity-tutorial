f489d3e11e395fb77fdc34b513ddd455913b59b7
Troy Mathis <troy.mathis00@gmail.com>
2023-06-03 18:14:43 -0500

    cn function for tailwind optimization, wokring on layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Paragraph from './components/ui/Paragraph'
+import Paragraph from '../components/ui/Paragraph'
 
 export default function Home() {
   return (
     <main className='bg-red-500'>
-      <Paragraph/>
+      
     </main>
   )
 }
--- src/components/ui/LargeHeading.tsx
+++ src/components/ui/LargeHeading.tsx
@@ -1,6 +1,6 @@
 import { HTMLAttributes, forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "@/app/lib/utils";
+import { cn } from "@/lib/utils";
 
 const headingVariants = cva(
   "text-black dark:text-white text-center lg:text-left font-extrabold leading-tight",
