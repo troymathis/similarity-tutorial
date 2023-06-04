741355428ebf6fb2f7a7958880dac9afbb76243b
Troy Mathis <troy.mathis00@gmail.com>
2023-06-03 19:00:30 -0500

    working on button and a bunch of navbar components for the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,30 @@
-import '@/styles/globals.css'
-import { Inter } from 'next/font/google'
-import { cn } from '@/lib/utils'
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Providers from "@/components/Providers";
 
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn(
-      "bg-white text-slate-900 antialiased", inter.className
-    )}>
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={cn("bg-white text-slate-900 antialiased", inter.className)}
+    >
+      <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
+        <Providers>{children}
+
+
+        <Navbar />
+        </Providers>
+
+        {/* Allow for more height on mobile devices */}
+        <div className='h-40 md:hidden'/>
+      </body>
     </html>
-  )
+  );
 }
